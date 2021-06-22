22630711d6c1ef1be850a7d83c67c79292215604
Roland <rolandbasset@hotmail.fr>
2021-06-22 11:18:25 +0200

    hello with db and templates

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ from flask import request, redirect, make_response, abort, session, url_for, fla
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 # --- Forms ---
 from flask_wtf import Form
@@ -13,6 +14,8 @@ from wtforms.validators import Required
 
 
 
+
+from werkzeug.security import generate_password_hash, check_password_hash
 # from flask.ext.script import Manager // depricated
 # from flask_script import Manager
 
@@ -34,11 +37,31 @@ bootstrap = Bootstrap(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_USERNAME'] = 'rbassetchercot@gmail.com'
+# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_PASSWORD'] = '9Uy195F5'
 
 db = SQLAlchemy(app)
 
-from werkzeug.security import generate_password_hash, check_password_hash
+mail = Mail(app)
 
+# --- MAILS
+
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+def send_email(to, subject, template, **kwargs):
+ msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+ sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+ msg.body = render_template(template + '.txt', **kwargs)
+ msg.html = render_template(template + '.html', **kwargs)
+ mail.send(msg)
+
+
+# -- MODELS
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -69,6 +92,16 @@ class NameForm(Form):
     submit = SubmitField('Submit')
 
 
+# --- MAIL FORM ---
+
+# app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['FLASKY_ADMIN'] = 'rolandbasset@hotmail.fr'
+@app.route('/mail', methods=['GET', 'POST'])
+def mail():
+    send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
+    return '<h1>MAIL</h1>'
+
+
 # --- ROUTES ---
 
 @app.route('/', methods=['GET', 'POST'])
@@ -88,7 +121,7 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
-        return redirect(url_for('index'))
+        # return redirect(url_for('index'))
     # return render_template('index.html', form=form, name=session.get('name'), known = session.get('known', False))
     return '<h1>Hello %s!</h1>' % getName.username
 
